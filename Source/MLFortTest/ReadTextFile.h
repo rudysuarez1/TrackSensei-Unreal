ab49c320319c7fab8dcd25d5cad61bf3255b091d
danfel45 <danfel45@gmail.com>
2025-04-24 11:42:06 -0700

    Allowed for imported models of scans to be driven on by the cars, trained model slightly, and edited the mesh of the test scan

---
+++ Source/MLFortTest/ReadTextFile.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include <Misc/FileHelper.h>
+#include "ReadTextFile.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class MLFORTTEST_API UReadTextFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+		
+public:
+
+	UFUNCTION(BlueprintCallable)
+	static FString ReadStringFromFile(FString FilePath, bool& bOutSuccess, FString& OutInfoMessage);
+
+
+
+	
+};
